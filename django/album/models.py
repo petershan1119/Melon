58e1d1f0795fa2fabaee2af130d42aa512e07659
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-07 12:49:02 +0900

    Artist, Album, Song 모델 구축

--- django/album/models.py
+++ django/album/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from artist.models import Artist
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ManyToManyField(Artist)
+
+    def __str__(self):
+        return self.title
--- django/song/models.py
+++ django/song/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from album.models import Album
+from artist.models import Artist
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ManyToManyField(Artist, through='ArtistSong')
+
+    def __str__(self):
+        return self.title
+
+
+class ArtistSong(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    demo_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'artist: {self.artist} - song: {self.song.title}'
