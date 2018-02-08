71059638e82580c981edff0d2ff96d02a0d6e39e
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-08 11:18:54 +0900

    Album에 __str__ 정의

--- django/album/models.py
+++ django/album/models.py
@@ -3,9 +3,25 @@ from django.db import models
 from artist.models import Artist
 
 
+# class Album(models.Model):
+#     title = models.CharField(max_length=255)
+#     artist = models.ManyToManyField(Artist)
+#
+#     def __str__(self):
+#         return self.title
+
+
 class Album(models.Model):
-    title = models.CharField(max_length=255)
-    artist = models.ManyToManyField(Artist)
+    title = models.CharField('앨범명', max_length=100)
+    img_cover = models.ImageField('커버 이미지', upload_to='album', blank=True)
+    artists = models.ManyToManyField(Artist, verbose_name='아티스트 목록')
+    release_date = models.DateField()
+    # genre = models.CharField(max_length=100)
+
+    @property
+    def genre(self):
+        return f''
 
     def __str__(self):
-        return self.title
+        return f'{self.title} {[item for item in self.artists.all().values_list("name", flat=True)]}'
+    # ', '.join([artist.name for artist in a1.artists.all()]) or ', '.join(a1.artist.values_list('name', flat=True))
--- django/song/models.py
+++ django/song/models.py
@@ -8,14 +8,16 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     artist = models.ManyToManyField(Artist, through='ArtistSong')
+    # artist = models.ManyToManyField(Artist, through='ArtistSong', related_name='song')
 
     def __str__(self):
         return self.title
 
 
 class ArtistSong(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='relation_artist')
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='relation_song')
+    # album = models.ForeignKey(Album, on_delete=models.CASCADE)
     demo_date = models.DateTimeField(null=True)
 
     def __str__(self):
