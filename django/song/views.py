3bdd9636d860699f1e8e3424db6132198de349ca
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-19 10:57:20 +0900

    song_search 함수 추가

--- django/song/views.py
+++ django/song/views.py
@@ -7,3 +7,18 @@ def song_list(request):
         'songs': songs,
     }
     return render(request, 'song/song_list.html', context)
+
+
+def song_search(request, title):
+    """
+    사용할 URL: song/search
+    사용할 Template: templates/song/song_search.html
+        form안에
+            input한개, button한개 배치
+    """
+    # titles = [item.title for item in Song.objects.all()]
+    # context = {
+    #     'titles': titles,
+    # }
+    # return render(request, 'song/song_search.html', context)
+    pass
