d9a37675eda0efbe7446a2200d3d7a4df28b9333
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-20 10:58:33 +0900

    3개 탐색 항목 구성

--- django/song/views.py
+++ django/song/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from song.models import Song
@@ -36,15 +37,39 @@ def song_search(request):
     3. context dict를 만들고 'songs'키에 songs변수를 할당
     4. render의 3번째 인수로 context 전달
     5. templates에 전달된 'songs'를 출력
+
+    Song과 연결된 Artist의 name에 keyword가 포함되는 경우
+    Song과 연결된 Album의 title에 keyword가 포함되는 경우
+        를 모두 포함(or -> Q object)하는 쿼리셋을 'songs'에 할당
+
+    songs_from_artists
+    songs_from_albums
+    songs_from_title
+    위 세 변수에 위의 조건 3개에 부합하는 쿼리셋을 각각 전달
+    세 변수를 이용해 검색 결과를 3단으로 분리해서 출력
+        -> 아티스트로 검색한 노래 결과, 앨범으로 검색한 노래 결과, 제목으로 검색한 노래 결과
     """
     context = {}
     if request.method == 'POST':
         keyword = request.POST['keyword'].strip()
-        songs = Song.objects.filter(title__contains=keyword)
-        if not songs or not keyword:
-            context['form_error'] = "찾는 노래가 없습니다."
+        songs_from_artists = Song.objects.filter(album__artists__name__contains=keyword)
+        songs_from_albums = Song.objects.filter(album__title__contains=keyword)
+        songs_from_title = Song.objects.filter(title__contains=keyword)
+        # songs = Song.objects.filter(
+        #     Q(album__title__contains=keyword)|
+        #     Q(album__artists__name__contains=keyword)|
+        #     Q(title__contains=keyword)
+        # ).distinct()
+        if not songs_from_artists or not keyword:
+            context['form_error1'] = "찾는 아티스트의 노래가 없습니다."
         else:
-            context = {
-                'songs': songs,
-            }
+            context['songs_from_artists'] = songs_from_artists
+        if not songs_from_albums or not keyword:
+            context['form_error2'] = "찾는 앨범의 노래가 없습니다."
+        else:
+            context['songs_from_albums'] = songs_from_albums
+        if not songs_from_title or not keyword:
+            context['form_error3'] = "찾는 타이틀의 노래가 없습니다."
+        else:
+            context['songs_from_title'] = songs_from_title
     return render(request, 'song/song_search.html', context)
